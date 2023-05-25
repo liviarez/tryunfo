951a36f214133c6c44a6b5af303bd7675d4f42f7
liviarez <mrezende.livia@gmail.com>
2023-05-25 09:37:44 -0300

    Feat: Req 10 and 11. Implements FIlter

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
       hasTrunfo: false,
       isSaveButtonDisabled: true,
       allCards: [],
+      filteredCards: [],
     };
   }
 
@@ -94,12 +95,13 @@ class App extends React.Component {
   onDeleteButtonClick = (cardName) => {
     const { allCards } = this.state;
     // Expects to filter all cards ins the array but the selected one
-    const updatedAllCards = allCards.filter((card) => card
-      .cardName !== cardName.target.id);
+    const updatedAllCards = allCards.filter(
+      (card) => card.cardName !== cardName.target.id,
+    );
     // Expects to find matching Super Trunfo in HTML
     const isRemovedCardTrunfo = cardName.target
-      .previousElementSibling
-      .lastChild.textContent === 'Super Trunfo';
+      .previousElementSibling.lastChild.textContent
+      === 'Super Trunfo';
     // Expects hasTrunfo to be false if found the Super Trunfo in HTML
     if (isRemovedCardTrunfo) {
       this.setState(() => ({
@@ -110,6 +112,30 @@ class App extends React.Component {
     this.setState(() => ({
       allCards: updatedAllCards,
     }));
+  };
+
+  filterByCardName = ({ target }) => {
+    const { allCards } = this.state;
+    const filteredCard = allCards.filter((card) => card.cardName.includes(target.value));
+
+    this.setState(() => ({
+      allCards: filteredCard,
+    }));
+  };
+
+  filterByRarity = ({ target }) => {
+    const { allCards } = this.state;
+
+    this.setState(() => {
+      if (target.value === 'todas') {
+        return { filteredCards: allCards };
+      }
+      return {
+        filteredCards: allCards.filter(
+          (card) => card.cardRare === target.value,
+        ),
+      };
+    });
   };
 
   onInputChange = ({ target }) => {
@@ -139,11 +165,12 @@ class App extends React.Component {
       cardTrunfo,
       allCards,
       isSaveButtonDisabled,
+      filteredCards,
     } = this.state;
 
     return (
       <div>
-        <h1>Tryunfo</h1>
+        <h1 className="title">Tryunfo</h1>
         <Form
           { ...this.state }
           cardName={ cardName }
@@ -158,39 +185,90 @@ class App extends React.Component {
           isSaveButtonDisabled={ isSaveButtonDisabled }
           onSaveButtonClick={ this.onSaveButtonClick }
         />
-        <Card { ...this.state } />
-        <div className="container">
-          <section className="Card__content">
-            {allCards.map((card) => (
-              <div key={ card.cardName }>
-                <Card
-                  cardName={ card.cardName }
-                  cardDescription={ card.cardDescription }
-                  cardAttr1={ card.cardAttr1 }
-                  cardAttr2={ card.cardAttr2 }
-                  cardAttr3={ card.cardAttr3 }
-                  cardImage={ card.cardImage }
-                  cardRare={ card.cardRare }
-                  cardTrunfo={ card.cardTrunfo }
-                />
-                <button
-                  type="button"
-                  data-testid="delete-button"
-                  id={ card.cardName }
-                  onClick={ this.onDeleteButtonClick }
-                >
-                  Excluir
-                </button>
-              </div>
-            ))}
-          </section>
-          <section>{}</section>
+        <div>
+          <h1>TODAS AS CARTAS</h1>
+          <label>
+            Filtros de busca:
+            <input
+              type="text"
+              data-testid="name-filter"
+              placeholder="Nome da Carta"
+              onChange={ this.filterByCardName }
+            />
+          </label>
+          <select
+            data-testid="rare-filter"
+            onChange={ this.filterByRarity }
+            /* disabled={ disabledFilters } */
+          >
+            <option value="todas">Todas</option>
+            <option value="normal">Normal</option>
+            <option value="raro">Raro</option>
+            <option value="muito raro">Muito Raro</option>
+          </select>
         </div>
+        <Card { ...this.state } />
+        {filteredCards.length > 0 ? (
+          <div className="container">
+            <section className="Card__content">
+              {filteredCards.map((card) => (
+                <div key={ card.cardName }>
+                  <Card
+                    cardName={ card.cardName }
+                    cardDescription={ card.cardDescription }
+                    cardAttr1={ card.cardAttr1 }
+                    cardAttr2={ card.cardAttr2 }
+                    cardAttr3={ card.cardAttr3 }
+                    cardImage={ card.cardImage }
+                    cardRare={ card.cardRare }
+                    cardTrunfo={ card.cardTrunfo }
+                  />
+                  <button
+                    type="button"
+                    data-testid="delete-button"
+                    id={ card.cardName }
+                    onClick={ this.onDeleteButtonClick }
+                  >
+                    Excluir
+                  </button>
+                </div>
+              ))}
+            </section>
+          </div>
+        ) : (
+          <div className="container">
+            <section className="Card__content">
+              {allCards.map((card) => (
+                <div key={ card.cardName }>
+                  <Card
+                    cardName={ card.cardName }
+                    cardDescription={ card.cardDescription }
+                    cardAttr1={ card.cardAttr1 }
+                    cardAttr2={ card.cardAttr2 }
+                    cardAttr3={ card.cardAttr3 }
+                    cardImage={ card.cardImage }
+                    cardRare={ card.cardRare }
+                    cardTrunfo={ card.cardTrunfo }
+                  />
+                  <button
+                    type="button"
+                    data-testid="delete-button"
+                    id={ card.cardName }
+                    onClick={ this.onDeleteButtonClick }
+                  >
+                    Excluir
+                  </button>
+                </div>
+              ))}
+            </section>
+          </div>
+        )}
       </div>
     );
   }
 }
-
 // ... spread operator - unpacks elements from arrays or objects, allowing you to combine or copy them easily.
 //
 export default App;
+
+// tem como separar as funções em arquivo separado pra reduzir o numero de linhas e organizar o codigo?
